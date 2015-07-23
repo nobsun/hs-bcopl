e3860e3d1b7f27cf0b33aced0a0bc978a39281d8
Nobuo Yamashita <nobsun@sampou.org>
2015-07-23 21:59:44 +0900

    Show instance

--- src/Language/BCoPL/CompareNat.hs
+++ src/Language/BCoPL/CompareNat.hs
@@ -22,45 +22,40 @@ data LessThan1 (n1 :: Nat) (n2 :: Nat) where
           -> LessThan1 n1 n2 -> LessThan1 n2 n3
           -> LessThan1 n1 n3
 
-instance (Show (Nat' n)) => Show (LessThan1 n (S n)) where
+instance Show (LessThan1 n1 n2) where
   show (LSucc1 n)
-    = unwords [show n,"is","less","than",show (S' n),"by","L-Succ"
-              ,"{","}"]
-{-
-instance (Show (Nat' n1),Show (Nat' n2),Show (Nat' n3)
-         ,Show (LessThan1 n1 n2),Show (LessThan1 n2 n3)
-         ) => Show (LessThan1 n1 n3) where
+    = unwords [show n,"is","less","than",show (S' n),"by","L-Succ","{","}"]
   show (LTrans1 n1 n2 n3 j1 j2)
-    = unwords [show n1,"is","less","than",show n3,"by","L-Trans"
-              "{",show j1,";",show j2,"}"]
--}
+    = unwords [show n1,"is","less","than",show n3,"by","L-Trans","{"
+              ,show j1,";",show j2
+              ,"}"
+              ]
 
 data LessThan2 (n1 :: Nat) (n2 :: Nat) where
   LZero2 :: Nat' n -> LessThan2 Z (S n)
   LSuccSucc2 :: Nat' n1 -> Nat' n2 -> LessThan2 n1 n2 -> LessThan2 (S n1) (S n2)
 
-instance (Show (Nat' n)) => Show (LessThan2 Z (S n)) where
+instance Show (LessThan2 n1 n2) where
   show (LZero2 n)
-    = unwords [show Z',"is","less","than",show (S' n),"by","L-Zero"
-              ,"{","}"]
-
-instance (Show (Nat' n1),Show (Nat' n2),Show (LessThan2 n1 n2))
-  => Show (LessThan2 (S n1) (S n2)) where
+    = unwords [show Z',"is","less","than",show (S' n),"by","L-Zero","{","}"]
   show (LSuccSucc2 n1 n2 j)
-    = unwords [show (S' n1),"is","less","than",show (S' n2),"by","L-Zero"
-              ,"{",show j,"}"]
+    = unwords [show (S' n1),"is","less","than",show (S' n2),"by","L-SuccSucc","{"
+              ,show j
+              ,"}"
+              ]
 
 data LessThan3 (n1 :: Nat) (n2 :: Nat) where
   LSucc3 ::  Nat' n -> LessThan3 n (S n)
   LSuccR3 :: Nat' n1 -> Nat' n2 -> LessThan3 n1 n2 -> LessThan3 n1 (S n2)
 
-instance (Show (Nat' n1),Show (Nat' n2)
-         ,Show (LessThan3 n1 n2)
-         ) => Show (LessThan3 n1 (S n2)) where
+instance Show (LessThan3 n1 n2) where
+  show (LSucc3 n) 
+    = unwords [show n,"is","less","than",show (S' n),"by","L-Succ","{","}"]
   show (LSuccR3 n1 n2 j)
-    = unwords [show (S' n1),"is","less","than",show (S' n2),"by","LSuccR"
-              ,"{",show j,"}"]
-         
+    = unwords [show n1,"is","less","than",show (S' n2),"by","L-SuccR","{"
+              ,show j
+              ,"}"
+              ]
 
 -- 練習問題 1.5
 
@@ -82,5 +77,5 @@ ex010502_2 :: LessThan2 (S(S Z)) (S(S(S(S Z))))
 ex010502_2 =  LSuccSucc2 (S' Z') (S'(S'(S' Z')))
                          (LSuccSucc2 Z' (S'(S' Z')) (LZero2 (S' Z')))
 
-ex010503_3 :: LessThan3 (S(S Z)) (S(S(S(S Z))))
-ex010503_3 =  LSuccR3 (S'(S' Z')) (S'(S'(S' Z'))) (LSucc3 (S'(S' Z')))
+ex010502_3 :: LessThan3 (S(S Z)) (S(S(S(S Z))))
+ex010502_3 =  LSuccR3 (S'(S' Z')) (S'(S'(S' Z'))) (LSucc3 (S'(S' Z')))
