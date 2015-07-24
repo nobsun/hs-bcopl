7adf5c719ba68be4ad58b603fb1afbe6fb9ddb94
Nobuo Yamashita <nobsun@sampou.org>
2015-07-24 10:36:30 +0900

    Read instance

--- src/Language/BCoPL/Peano.hs
+++ src/Language/BCoPL/Peano.hs
@@ -1,9 +1,14 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 module Language.BCoPL.Peano where
+
+import Data.Char
+import Data.List
+import Text.ParserCombinators.ReadP
 
 data Nat = Z 
          | S Nat deriving (Eq,Ord,Read)
@@ -19,3 +24,15 @@ data Nat' n where
 instance Show (Nat' n) where
   show Z'     = "Z"
   show (S' n) = "S("++show n++")"
+
+instance Read (Nat' Z) where
+  readsPrec _ s = case trim s of "Z" -> [(Z',"")]
+
+instance Read (Nat' n) => Read (Nat' (S n)) where
+  readsPrec _ s = case trim s of
+    'S':'(':rs -> [(S' (read (init rs)),"")]
+    'S':rs     -> [(S' (read rs),"")]
+    _          -> []
+
+trim :: String -> String
+trim = dropWhileEnd isSpace . dropWhile isSpace
