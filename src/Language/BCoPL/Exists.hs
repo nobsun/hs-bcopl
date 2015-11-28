094bf9a90363c2eed33359564b8ab28cce44c2c4
Nobuo Yamashita <nobsun@sampou.org>
2015-11-29 00:46:45 +0900

    add Language.BCoPL.Exists

---
+++ src/Language/BCoPL/Exists.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+module Language.BCoPL.Exists where
+
+data Exists (s :: k -> *) (p :: k -> *) where
+  ExIntro :: s a -> p a -> Exists k p
