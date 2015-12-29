02ab32a0e8d2f6e6c518d0f71571480147964fd6
Nobuo Yamashita <nobsun@sampou.org>
2015-12-29 10:02:04 +0900

    proof theory 2.1-2.10

--- src/Language/BCoPL/MetaTheory/Nat.hs
+++ src/Language/BCoPL/MetaTheory/Nat.hs
@@ -23,6 +23,14 @@ import Language.BCoPL.Equiv
 import Language.BCoPL.Exists
 
 -- ---------------------------------------------
+-- $setup
+-- >>> type family (m :: Nat) :+ (n :: Nat) :: Nat
+-- >>> type instance Z :+ n = n
+-- >>> type instance (S m) :+ n = S (m :+ n)
+-- >>> type family (m :: Nat) :* (n :: Nat) :: Nat
+-- >>> type instance     Z :* n = Z
+-- >>> type instance (S m) :* n = (m :* n) :+ n
+-- >>> let {add :: Nat' n1 -> Nat' n2 -> Nat' (n1 :+ n2); add Z' n2 = n2; add (S' n1) n2 = S' (add n1 n2) }
 
 -- 定理 2.1 加法単位元
 
@@ -56,13 +64,9 @@ eqPlusZero Z' n' (PZero _)  = eqZeroPlus Z' n' (PZero n')
 eqPlusZero (S' n) _ (PSucc _ _ n' j) = Cong (eqPlusZero n n' j)
 
 -- 定理 2.2 加法唯一性
--- >>> type family (m :: Nat) :+ (n :: Nat) :: Nat
--- >>> type instance Z :+ n = n
--- >>> type instance (S m) :+ n = S (m :+ n)
--- >>> type family (m :: Nat) :* (n :: Nat) :: Nat
--- >>> type instance     Z :* n = Z
--- >>> type instance (S m) :* n = (m :* n) :+ n
--- >>> let {add :: Nat' n1 -> Nat' n2 -> Nat' (n1 :+ n2); add Z' n2 = n2; add (S' n1) n2 = S' (add n1 n2) }
+
+-- | 加法唯一性
+--
 -- >>> plusUnique Z' (S'(S' Z')) (S'(S' Z')) (add (S' Z') (S' Z')) (PZero (S'(S' Z'))) (PZero (add (S' Z') (S' Z')))
 -- >>> Refl
 
@@ -76,6 +80,10 @@ plusUnique (S' n1) n2 (S' n3) (S' n4) (PSucc _ _ _ j) (PSucc _ _ _ j')
 
 -- 定理 2.3 加法閉包性
 
+-- | 加法閉包性
+--
+-- >>> plusClosure Z' (S' Z)
+--
 plusClosure :: Nat' n1 -> Nat' n2 -> Exists Nat' (Plus n1 n2)
 plusClosure Z' n2       = ExIntro n2 (PZero n2)
 plusClosure (S' n1) n2 = case plusClosure n1 n2 of
@@ -113,12 +121,10 @@ plusAssocR (S' _) _ _ _ _ (PSucc n1 n2 n4 j) (PSucc _ n3 n5 k) = case plusAssocR
 newtype PlusAssocL n1 n2 n3 n5 n6 = PlusAssocL (Plus n1 n2 n6,Plus n6 n3 n5)
 
 plusAssocL :: Nat' n1 -> Nat' n2 -> Nat' n3 -> Nat' n4 -> Nat' n5
-           -> Plus n2 n3 n4 -> Plus n1 n4 n5
-           -> Exists Nat' (PlusAssocL n1 n2 n3 n5)
-plusAssocL Z' n2 n3 n4 n5 j (PZero _) = ExIntro n2 (PlusAssocL (PZero n2,j))
-plusAssocL (S' _) n2 n3 n4 _ j (PSucc n1 _ n5 k) = case plusAssocL n1 n2 n3 n4 n5 j k of
-  ExIntro n6 (PlusAssocL (j',k'))
-    -> ExIntro (S' n6) (PlusAssocL (PSucc n1 n2 n6 j',PSucc n6 n3 n5 k'))
+            -> Plus n2 n3 n4 -> Plus n1 n4 n5
+            -> Exists Nat' (PlusAssocL n1 n2 n3 n5)
+plusAssocL n1 n2 n3 n4 n5 p1 p2 = case plusAssocR n3 n2 n1 n4 n5 (plusComm n2 n3 n4 p1) (plusComm n1 n4 n5 p2) of
+  ExIntro n6 (PlusAssocR (p5,p6)) -> ExIntro n6 (PlusAssocL (plusComm n2 n1 n6 p5, plusComm n3 n6 n5 p6))
 
 -- 定理 2.6 乗法唯一性
 
@@ -164,12 +170,10 @@ succTimes = TSucc
 timesSucc :: Nat' n1 -> Nat' n2 -> Nat' n3 -> Nat' n4 -> Times n1 n2 n3 -> Plus n1 n3 n4 -> Times n1 (S n2) n4
 timesSucc Z' n2 _  _ (TZero _) (PZero _) = TZero (S' n2)
 timesSucc (S' n1) n2 n3 (S' n4) (TSucc _ _ n5 _ t1 p1) (PSucc _ _ _ p2) = case plusClosure n1 n5 of
-  ExIntro n6 p3 -> case timesSucc n1 n2 n5 n6 t1 p3 of
-    t2 -> case plusAssocL n1 n2 n5 n3 n4 p1 p2 of
-      ExIntro n7 (PlusAssocL (p4,p5)) -> case plusComm n1 n2 n7 p4 of
-        p6 -> case plusAssocR n2 n1 n5 n7 n4 p6 p5 of
-          ExIntro n8 (PlusAssocR (p7,p8)) -> case plusUnique n1 n5 n6 n8 p3 p7 of
-            Refl -> TSucc n1 (S' n2) n6 (S' n4) t2 (PSucc n2 n6 n4 p8)
+  ExIntro n6 p3 -> case plusAssocL n1 n2 n5 n3 n4 p1 p2 of
+    ExIntro n7 (PlusAssocL (p4,p5)) -> case plusAssocR n2 n1 n5 n7 n4 (plusComm n1 n2 n7 p4) p5 of
+      ExIntro n8 (PlusAssocR (p7,p8)) -> case plusUnique n1 n5 n6 n8 p3 p7 of
+        Refl -> TSucc n1 (S' n2) n6 (S' n4) (timesSucc n1 n2 n5 n6 t1 p3) (PSucc n2 n6 n4 p8)
 
 -- 定理 2.10 乗法結合律
 
@@ -180,30 +184,34 @@ timesAssocR :: Nat' n1 -> Nat' n2 -> Nat' n3 -> Nat' n4 -> Nat' n5
             -> Exists Nat' (TimesAssocR n1 n2 n3 n5)
 timesAssocR Z' n2 n3 _ _ (TZero _) (TZero _) = case timesClosure n2 n3 of
   ExIntro n6 t1 -> ExIntro n6 (TimesAssocR (t1,TZero n6))
-timesAssocR (S' n1) n2 n3 n4 n5 (TSucc _ _ n6 _ t1 p1) t2 = case timesClosure n6 n3 of
-  ExIntro n7 t3 -> case timesAssocR n1 n2 n3 n6 n7 t1 t3 of
-    _ -> undefined
+timesAssocR (S' n1) n2 n3 n4 n5 (TSucc _ _ n6 _ t2 p2) t1 = case timesClosure n6 n3 of
+  ExIntro n7 t3 -> case timesAssocR n1 n2 n3 n6 n7 t2 t3 of
+    ExIntro n8 (TimesAssocR (t41,t42)) -> case timesDistribL n3 n2 n6 n4 n5 p2 (timesComm n4 n3 n5 t1) of
+      ExIntro2 (Both (n9,n10)) (DistribL (t51,t52,p5)) -> case timesUnique n2 n3 n8 n9 t41 (timesComm n3 n2 n9 t51) of
+        Refl -> case timesUnique n6 n3 n7 n10 t3 (timesComm n3 n6 n10 t52) of
+          Refl -> ExIntro n8 (TimesAssocR (t41,TSucc n1 n8 n7 n5 t42 p5))
+
+newtype TimesAssocL n1 n2 n3 n5 n6 = TimesAssocL (Times n1 n2 n6,Times n6 n3 n5)
+
+timesAssocL :: Nat' n1 -> Nat' n2 -> Nat' n3 -> Nat' n4 -> Nat' n5
+            -> Times n2 n3 n4 -> Times n1 n4 n5
+            -> Exists Nat' (TimesAssocL n1 n2 n3 n5)
+timesAssocL n1 n2 n3 n4 n5 t1 t2 = case timesAssocR n3 n2 n1 n4 n5 (timesComm n2 n3 n4 t1) (timesComm n1 n4 n5 t2) of
+  ExIntro n6 (TimesAssocR (t3,t4)) -> ExIntro n6 (TimesAssocL (timesComm n2 n1 n6 t3,timesComm n3 n6 n5 t4))
+
+-- 左分配律
 
 newtype Both m n = Both (Nat' m, Nat' n)
 newtype DistribL n1 n2 n3 n4 n5 n6 = DistribL (Times n1 n2 n5, Times n1 n3 n6, Plus n5 n6 n4)
 
-{- -}
 timesDistribL :: Nat' n1 -> Nat' n2 -> Nat' n3 -> Nat' n4 -> Nat' n5
               -> Plus n2 n3 n4 -> Times n1 n4 n5
               -> Exists2 Both (DistribL n1 n2 n3 n5)
 timesDistribL Z'      n2 n3 _  _  _  (TZero _) = ExIntro2 (Both (Z',Z')) (DistribL (TZero n2, TZero n3, PZero Z'))
-timesDistribL (S' n1) n2 n3 n4 n5 p1 (TSucc _ _ n6 _ t2 p2) = case timesDistribL n1 n2 n3 n4 n6 p1 t2 of
-  ExIntro2 (Both (n8,n9)) (DistribL (t3,t4,p3)) -> undefined
--- -}
+timesDistribL (S' n1) n2 n3 n4 n5 p0 (TSucc _ _ n6 _ t1 p1) = case timesDistribL n1 n2 n3 n4 n6 p0 t1 of
+  ExIntro2 (Both (n7,n8)) (DistribL (t21,t22,p2)) -> case plusAssocL n4 n7 n8 n6 n5 p2 p1 of
+    ExIntro n9 (PlusAssocL (p31,p32)) -> case plusAssocR n3 n2 n7 n4 n9 (plusComm n2 n3 n4 p0) p31 of
+      ExIntro n10 (PlusAssocR (p41,p42)) -> case plusAssocR n10 n3 n8 n9 n5 (plusComm n3 n10 n9 p42) p32 of
+        ExIntro n11 (PlusAssocR (p51,p52))
+          -> ExIntro2 (Both (n10,n11)) (DistribL (TSucc n1 n2 n7 n10 t21 p41,TSucc n1 n3 n8 n11 t22 p51,p52))
 
-{-
-   a'*b=d, d*c=e
-=> { tsucc, clos }
-   a*b=f, f*c=g
-=> { hypos }
-   b*c=h ,a*h=g
-=> { tsucc }
-   b*c=h,a'*h=e
--- -}
-
-newtype TimesAssocL n1 n2 n3 n5 n6 = TimesAssocL (Plus n1 n2 n6,Plus n6 n3 n5)
